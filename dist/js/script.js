834b5f1858f50cf5751c2ef18429f7c1479ead62
kolorg7 <pavelkorol3v@gmail.com>
2021-04-02 07:45:04 +0300

    v1.1

--- dist/js/script.js
+++ dist/js/script.js
@@ -45,4 +45,10 @@ $(document).ready(function(){
             $('.pageup').fadeOut();
         }
     });
+
+    $("a[href=#header]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
 });
