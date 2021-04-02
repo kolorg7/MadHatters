accfd86546b3a1fb7e1a386e750920b9150cfb8a
kolorg7 <pavelkorol3v@gmail.com>
2021-04-02 05:33:11 +0300

    v1

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,40 @@
 $(document).ready(function(){
     new WOW().init();
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+        });
+    };
+
+    validateForms('#newsletter-form');
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, #thanks').fadeOut('slow');
+    });
 });
